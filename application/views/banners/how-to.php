600e7042013efd79f431d4f1b7ddd18f5b7332fa
poigarcia <edilitogarcia@gmail.com>
2020-05-21 17:09:28 -0400

    made fixed card-item

--- application/views/banners/how-to.php
+++ application/views/banners/how-to.php
@@ -1,20 +1,20 @@
 <div class="container padding-zero-sm margin-bottom-15">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 division-inner text-center">
 		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-			<h3 class="zero-gap"><i class="color-theme fa fa-pagelines valign-middle"></i></h3>
+			<h3 class="zero-gap margin-bottom-5"><i class="color-theme fa fa-pagelines valign-middle"></i></h3>
 			<p>Freshly picked</p>
 		</div>
 		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-			<h3 class="zero-gap"><i class="color-theme fa fa-truck valign-middle"></i></h3>
+			<h3 class="zero-gap margin-bottom-5"><i class="color-theme fa fa-calendar-check-o valign-middle"></i></h3>
 			<p>Same day delivery</p>
 		</div>
 		<div class="col-xs-12 visible-xs"><br></div>
 		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-			<h3 class="zero-gap"><i class="color-theme fa fa-money valign-middle"></i></h3>
+			<h3 class="zero-gap margin-bottom-5"><i class="color-theme fa fa-truck valign-middle"></i></h3>
 			<p>Cash-On-Delivery</p>
 		</div>
 		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
-			<h3 class="zero-gap"><i class="color-theme fa fa-thumbs-o-up valign-middle"></i></h3>
+			<h3 class="zero-gap margin-bottom-5"><i class="color-theme fa fa-thumbs-o-up valign-middle"></i></h3>
 			<p>Support local farmers!</p>
 		</div>
 	</div>
