6aeb88e658593f302908569d516b1e0c55683a30
Eddie <eddie@ypdigital.com.au>
2020-05-30 13:53:02 +0800

    fixes

---
+++ application/views/templates/dashboard/global/cart.php
@@ -0,0 +1,102 @@
+
+<!-- Main content -->
+<section class="content pt-2">
+	<!-- Default box -->
+	<div class="card">
+		<div class="card-header">
+			<h3 class="card-title">Cart</h3>
+			<div class="card-tools">
+				<a href="/" class="btn btn-sm btn-primary">
+					<i class="fas fa-shopping-bag"></i> Continue Shopping
+				</a>
+				<!-- <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+					<i class="fas fa-times"></i>
+				</button> -->
+			</div>
+		</div>
+		<div class="card-body p-0">
+			<table class="table table-striped projects">
+				<thead>
+					<tr>
+						<th style="width: 1%;">
+							#
+						</th>
+						<th style="width: 20%;">
+							Product Name
+						</th>
+						<th style="width: 30%;">
+							Cart Items
+						</th>
+						<th>
+							Progress
+						</th>
+						<th style="width: 8%;" class="text-center">
+							Status
+						</th>
+						<th style="width: 20%;"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							#
+						</td>
+						<td>
+							<a>
+								AdminLTE v3
+							</a>
+							<br />
+							<small>
+								Ordered 01.01.2019
+							</small>
+						</td>
+						<td>
+							<ul class="list-inline">
+								<li class="list-inline-item">
+									<img alt="Avatar" class="table-avatar" src="<?php check_file_and_render(false, '37x37');?>" />
+								</li>
+								<li class="list-inline-item">
+									<img alt="Avatar" class="table-avatar" src="<?php check_file_and_render(false, '37x37');?>" />
+								</li>
+								<li class="list-inline-item">
+									<img alt="Avatar" class="table-avatar" src="<?php check_file_and_render(false, '37x37');?>" />
+								</li>
+								<li class="list-inline-item">
+									<img alt="Avatar" class="table-avatar" src="<?php check_file_and_render(false, '37x37');?>" />
+								</li>
+							</ul>
+						</td>
+						<td class="project_progress">
+							<div class="progress progress-sm">
+								<div class="progress-bar bg-green" role="progressbar" aria-volumenow="57" aria-volumemin="0" aria-volumemax="100" style="width: 57%;"></div>
+							</div>
+							<small>
+								57% Complete
+							</small>
+						</td>
+						<td class="project-state">
+							<span class="badge badge-success">Success</span>
+						</td>
+						<td class="project-actions text-right">
+							<a class="btn btn-primary btn-sm" href="#">
+								<i class="fas fa-folder"> </i>
+								View
+							</a>
+							<!-- <a class="btn btn-info btn-sm" href="#">
+								<i class="fas fa-pencil-alt"> </i>
+								Edit
+							</a> -->
+							<a class="btn btn-danger btn-sm" href="#">
+								<i class="fas fa-trash"> </i>
+								Cancel
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- /.card-body -->
+	</div>
+	<!-- /.card -->
+</section>
+<!-- /.content -->
